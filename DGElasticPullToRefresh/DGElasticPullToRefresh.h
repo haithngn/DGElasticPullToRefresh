a7655840455386fc3b78691666209fc97e2b875c
hai@leftcoastlogic.com <hai@leftcoastlogic.com>
2017-06-30 11:04:56 +0700

    - Add DGElasticPullToRefresh to new target

---
+++ DGElasticPullToRefresh/DGElasticPullToRefresh.h
@@ -0,0 +1,19 @@
+//
+//  DGElasticPullToRefresh.h
+//  DGElasticPullToRefresh
+//
+//  Created by Nguyen Thanh Hai on 6/30/17.
+//  Copyright © 2017 Danil Gontovnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DGElasticPullToRefresh.
+FOUNDATION_EXPORT double DGElasticPullToRefreshVersionNumber;
+
+//! Project version string for DGElasticPullToRefresh.
+FOUNDATION_EXPORT const unsigned char DGElasticPullToRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGElasticPullToRefresh/PublicHeader.h>
+
+
